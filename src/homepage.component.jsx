416c88ac616c4a02bb4206e8e0bb57d3aba58366
shamseena-dev <shamseenakarumarot@gmail.com>
2019-12-11 23:13:51 +0100

    Adding initial files

---
+++ src/homepage.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const HomePage = () =>(
+		<div className='homepage'>
+			<div className='directory-menu'>
+			    <div className='menu-item'>
+				  <div className='content'>
+				  	<h1 className='title'> BAGS </h1>
+				  	<span className='subtitle'> SHOP NOW </span>
+				  </div>
+				</div>
+				<div className='menu-item'>
+				  <div className='content'>
+				  	<h1 className='title'> WALLETS </h1>
+				  	<span className='subtitle'> SHOP NOW </span>
+				  </div>
+				</div>
+				<div className='menu-item'>
+				  <div className='content'>
+				  	<h1 className='title'> JACKETS </h1>
+				  	<span className='subtitle'> SHOP NOW </span>
+				  </div>
+				</div>
+				<div className='menu-item'>
+				  <div className='content'>
+				  	<h1 className='title'> SHOES </h1>
+				  	<span className='subtitle'> SHOP NOW </span>
+				  </div>
+				</div>
+			</div>
+		</div>
+ 	);
+export default HomePage;
